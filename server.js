9ad9877f6db018871be8bff8e892ae5ff1600f4b
nprati01 <nicole.e.prati@gmail.com>
2023-03-28 14:54:59 -0400

    Mount passport middleware

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session')
+var session = require('express-session')
+var passport = require('passport')
 
 require('dotenv').config()
 require('./config/database')
@@ -28,6 +29,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
