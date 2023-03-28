7532cda556d35788854b92d7edd237c3477daa28
adricastro1 <adricastro1196@gmail.com>
2023-03-28 11:48:59 -0400

    initial set up

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection
+
+db.on('connected', function() {
+    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
+  
+db.on('error', function(err) {
+    console.log("error stack:  \n", err.stack)
+})
