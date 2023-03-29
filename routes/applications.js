ab7d7105a25277599bdfaca2cae338a8d39714d4
Adamtpacz <adamtpacz@gmail.com>
2023-03-29 10:42:19 -0400

    Define delete route for applications

--- routes/applications.js
+++ routes/applications.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const applicationsCtrl = require('../controllers/applications')
+
 router.post('/dogs/:id/applications', applicationsCtrl.create)
 
 router.get('/dogs/:id/applications/new', applicationsCtrl.new)
+
+router.get('/:id', applicationsCtrl.delete)
 
 
 
